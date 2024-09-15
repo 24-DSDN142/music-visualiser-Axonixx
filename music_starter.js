d4923817e17cbd1249421b31efaa2bb50faf2f4e
Axonixx <treetopfox.ruby@gmail.com>
2024-09-15 20:25:41 +1200

    Background circle added

--- music_starter.js
+++ music_starter.js
@@ -1,3 +1,7 @@
+let imgCircle1;
+let imgCircle2;
+let imgGradient;
+let firstRun = true;
 
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
@@ -9,41 +13,78 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
   fill (0);
   angleMode(DEGREES);
 
+  if (firstRun){
+    imgCircle1 = loadImage('142 circle.png');
+    imgGradient = loadImage('gradient 1.png');
+    imgCircle2 = loadImage('circle.png');
+   firstRun = false;
+  }
+  
+
+
   let bassMap1 = map(bass, 0, 100, 1, 2);
   let bassMap2 = map(bass, 0, 100, 1, 1.5)
   let drumMap = map(drum, 0, 100, 0, 900);
+  let vocalMap = map(vocal, 0, 100, 0, 30);
+  let vocalMap2 = map(vocal, 0, 100, 0.5, 0.55);
+  
+
+  
 
 
+  push();
+  tint(255, 50);
+  translate(width/2, height/2);
+  rotate(vocalMap);
+  scale(vocalMap2);
+  image(imgCircle1, -1310.5, -1310.5);
 
+  pop();
+
+  push();
+  tint(255, 10);
+  translate(width/2, height/2);
+  rotate(vocalMap);
+  scale(vocalMap2);
+  image(imgCircle2, -1318.5, -1318.5);
+
+  pop();
+
+  push();
+  tint(255, 30);
+  scale(0.5);
+  image(imgGradient, 0, 0);
+  pop();
 
 
   stroke(
-    (drum + 50 > 100) ? drum - 50 : drum + 50, 
+    (other + 50 > 100) ? other - 50 : other + 50, 
     80, 80
   );
-  fill((drum + 50 > 100) ? drum - 50 : drum + 50, 
+  fill((other + 50 > 100) ? other - 50 : other + 50, 
   80, 80)
 
 
   strokeWeight(15);
-  line(width/2 -90, height/2+90, 200+drum, height/2-drumMap);
-  line(width/2 -90, height/2+90, 100, height/2-drumMap);
-  line(width/2 -90, height/2+90, 0-drum, height/2-drumMap);
+  line(width/2 -130, height/2+90, 200+drum, height/2-drumMap);
+  line(width/2 -130, height/2+90, 100, height/2-drumMap);
+  line(width/2 -130, height/2+90, 0-drum, height/2-drumMap);
 
-  stroke(drum, 80, 80);
+  stroke(other, 80, 80);
+
   line(width/2 +90, height/2+90, width-200-drum, height/2-drumMap);
   line(width/2 +90, height/2+90, width -100, height/2-drumMap);
   line(width/2 +90, height/2+90, width+drum, height/2-drumMap);
 
   strokeWeight(5);
   stroke(
-    (drum + 50 > 100) ? drum - 50 : drum + 50, 
+    (other + 50 > 100) ? other - 50 : other + 50, 
     10, 90
   );
 
-  line(width/2 -90, height/2+90, 200+drum, height/2-drumMap);
-  line(width/2 -90, height/2+90, 100, height/2-drumMap);
-  line(width/2 -90, height/2+90, 0-drum, height/2-drumMap);
+  line(width/2 -130, height/2+90, 200+drum, height/2-drumMap);
+  line(width/2 -130, height/2+90, 100, height/2-drumMap);
+  line(width/2 -130, height/2+90, 0-drum, height/2-drumMap);
 
   stroke(drum, 10, 90);
   line(width/2 +90, height/2+90, width-200-drum, height/2-drumMap);
@@ -73,10 +114,40 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
   noFill();
   translate(width/2-65, height/2+100);
   scale(bassMap1);
+
+  strokeWeight();
+  stroke(
+    (other + 50 > 100) ? other - 50 : other + 50, 
+    50, 90
+  );
+  ellipse(0, 0, 50, 50)
+
+  stroke(100);
+  strokeWeight(1);
   ellipse(0, 0, 50, 50);
- 
+
   pop();
   
+
+  push();
+
+  noStroke();
+  fill(100);
+  translate(width/2 -130, height/2+90);
+  scale(0.5);
+  rect(0, 0, 10, 10);
+  triangle(-5, -5, 5, -5, 0, -25);
+  triangle(-5, -5, -5, 5, -25, 0);
+  triangle(-5, 5, 5, 5, 0, 25);
+  triangle(5, -5, 5, 5, 25, 0);
+  
+  triangle(5, -5, -5, 5, -15, -15);
+  triangle(5, 5, -5, -5, -15, 15);
+  triangle(-5, 5, 5, -5, 15, 15);
+  triangle(-5, -5, 5, 5, 15, -15);
+
+  pop();
+
 
 }
 
